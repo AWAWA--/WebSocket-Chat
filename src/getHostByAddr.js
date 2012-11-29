3ae3b3e322396f6f0f0691f57608dba171b1757b
AWAWA <awawa.aw@gmail.com>
2012-11-29 23:03:08 +0900

    接続時のIPアドレスを元にホスト名を取得する処理を追加（ただし、dns.reverseでうまく取得できないことがあったので、ものすごくイケてない手で代替。しかもWindows限定）

---
+++ src/getHostByAddr.js
@@ -0,0 +1,34 @@
+/**
+ * dns.reverse でうまくホスト名が取得できないので、
+ * イケてない代替手段で取得（しかも動くのはWindowsのみ...）
+ * ※このファイル自体配置しなければ、dns.reverseで取得する処理が動く
+ */
+function(addr, defaultHostName, callback) {
+	var command = 'ping -n 1 -a ' + addr;
+	require('child_process').exec(command, function(error, stdout, stderr) {
+		// util.log(stdout);
+		if (error || stderr.length > 0) {
+			util.log('command error: command="'+command
+				+'" result='+JSON.stringify({error:error, stdout:stdout, stderr:stderr}));
+			callback(defaultHostName);
+			return;
+		}
+		var hostName;
+		try {
+			var result = stdout.toString().replace(/\r|\n/g, ' ').split(' ');
+			// util.log(result);
+			for (var i=0,l=result.length; i<l; i++) {
+				var item = result[i];
+				if (item == '') { continue; }
+				if (item == 'Pinging') { continue; } //WIndowsXP
+				hostName = item;
+				break;
+			};
+		} catch (e) {
+			util.log(e);
+			callback(defaultHostName);
+			return;
+		}
+		callback(hostName);
+	});
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 var express = require('express')
   , app = express()
+  , dns = require('dns')
   , fs = require('fs')
+  , path = require('path')
   , util = require('util');
 
 var http = require('http')
@@ -32,6 +34,7 @@ server.listen(port);
 
 var clientMap = {};
 
+//データキューの生成（過去ログとして使用）
 var Queue = function(queueSize) {
 	if (!!queueSize) { this.size = queueSize; }
 	this.queue = [];
@@ -65,122 +68,161 @@ Queue.prototype = {
 var msgQueue = new Queue(20);
 var figureQueue = new Queue(20);
 
-if (fs.existsSync(dataLogFile)) {
-	var dataStr = fs.readFileSync(dataLogFile, 'utf8');
-	if (dataStr == null || dataStr == '') { return; }
-	var data = JSON.parse(dataStr);
-	if (data.msgQueue) {
-		for (var i=0,l=data.msgQueue.length; i<l; i++) {
-			msgQueue.add(data.msgQueue[i]);
+
+//ログデータの読み込み
+(function() {
+	if (fs.existsSync(dataLogFile)) {
+		var dataStr = fs.readFileSync(dataLogFile, 'utf8');
+		if (dataStr == null || dataStr == '') { return; }
+		var data = JSON.parse(dataStr);
+		if (data.msgQueue) {
+			for (var i=0,l=data.msgQueue.length; i<l; i++) {
+				msgQueue.add(data.msgQueue[i]);
+			}
+			util.log('msgQueue loaded. datasize='+data.msgQueue.length);
 		}
-		util.log('msgQueue loaded. datasize='+data.msgQueue.length);
-	}
-	if (data.figureQueue) {
-		for (var i=0,l=data.figureQueue.length; i<l; i++) {
-			figureQueue.add(data.figureQueue[i]);
+		if (data.figureQueue) {
+			for (var i=0,l=data.figureQueue.length; i<l; i++) {
+				figureQueue.add(data.figureQueue[i]);
+			}
+			util.log('figureQueue loaded. datasize='+data.figureQueue.length);
 		}
-		util.log('figureQueue loaded. datasize='+data.figureQueue.length);
 	}
-}
+})()
+
+
+//ホスト名を取得するための関数を定義
+var getHostByAddr = (function() {
+	var getHostByAddrJS = path.dirname(__filename) + '/getHostByAddr.js';
+	var defaultFunc = function(address, defaultHostName, callback) {
+		dns.reverse(address, function(err, host) {
+			if (err) {
+				util.log('DNS reverse failed: address='+address+' err='+JSON.stringify(err));
+				callback(defaultHostName);
+				return;
+			}
+			if (host == null || host == '') {
+				callback(defaultHostName);
+			} else {
+				if (host.length == 1) {
+					callback(host[0]);
+				} else {
+					callback(host.toString());
+				}
+			}
+		});
+	};
+	if (fs.existsSync(getHostByAddrJS)) {
+		var dataStr = fs.readFileSync(getHostByAddrJS, 'utf8');
+		if (dataStr != null && dataStr != '') {
+			return eval('('+dataStr+')');
+		}
+	}
+	return defaultFunc;
+})()
+
 
 io.sockets.on('connection', function (socket) {
 
 	socket.on('chat start', function(data) {
-		//util.log('chat start: ' + JSON.stringify(data));
-		var userName = data.name;
-		if (userName.length > 100) {
-			userName = userName.substring(0, 100) + '...';
-		}
-		var userAgent = '' + socket.handshake.headers['user-agent'];
-		if (userAgent.length > 256) {
-			userAgent = userAgent.substring(0, 256) + '...';
-		}
-
-		var userData = {
-			id : socket.id,
-			name : userName,
-			host : socket.handshake.address.address,
-			addr : socket.handshake.address.address,
-			loginDate : new Date().getTime(),
-			userAgent : userAgent
-		};
-		util.log('<+> add connection: '+JSON.stringify(userData));
-
-		clientMap[socket.id] = userData;
-
-		socket.emit('chat setup', {
-			myData : userData,
-			users : (function() {
-				var userList = [];
-				for (var i in clientMap) {
-					userList.push(clientMap[i]);
-				}
-				return userList;
-			})(),
-			msgList : msgQueue.getAll(),
-			figureList : figureQueue.getAll()
-		});
-
-		socket.broadcast.emit('user add', {'users' : userData, reconnect: data.reconnect});
-
-		socket.on('message send', function(data) {
-			var  msgTarget = data.msgTarget;
-			var isReply = data.isReply;
-			var msg = data.msg;
-			if (msg.length > 2048) {
-				msg = msg.substring(0, 2048) + '...';
+		var address = socket.handshake.address.address;
+		getHostByAddr(address, '****', function(hostName) {
+			//util.log('chat start: ' + JSON.stringify(data));
+			var userName = data.name;
+			if (userName.length > 100) {
+				userName = userName.substring(0, 100) + '...';
+			}
+			var userAgent = '' + socket.handshake.headers['user-agent'];
+			if (userAgent.length > 256) {
+				userAgent = userAgent.substring(0, 256) + '...';
 			}
 
-			var client = clientMap[socket.id];
-
-			var sendMsg = {
-				'isPrivate' : (msgTarget != null && '' != msgTarget),
-				'msgTarget' : msgTarget,
-				'isReply' : isReply,
-				'time' : new Date().getTime(),
-				'id' : client.id,
-				'name' : client.name,
-				'host' : client.host,
-				'addr' : client.addr,
-				'msg' : '' + msg
+			var userData = {
+				id : socket.id,
+				name : userName,
+				host : hostName,
+				addr : address,
+				loginDate : new Date().getTime(),
+				userAgent : userAgent
 			};
+			util.log('<+> add connection: '+JSON.stringify(userData));
 
-			socket.emit('message push', sendMsg);
-			if (sendMsg.isPrivate) {
-				var targetSocket = io.sockets.socket(msgTarget);
-				if (clientMap[msgTarget] != null && targetSocket != null) {
-					var callbackCatched = false;
-					targetSocket.emit('message push', sendMsg, function(callbackData) {
-						callbackCatched = true;
-					});
-					setTimeout(function() {
-						//console.log('callbackCatched:'+callbackCatched);
-						if (!callbackCatched) {
-							socket.emit('error push', {
-								errorID : 'PRIVATEMSG_CALLBACK_UNCATCHED'
-							});
-						}
-					}, 10*1000);
-				} else {
-					socket.emit('error push', {
-						errorID : 'PRIVATEMSG_TARGET_NOT_EXIST'
-					});
+			clientMap[socket.id] = userData;
+
+			socket.emit('chat setup', {
+				myData : userData,
+				users : (function() {
+					var userList = [];
+					for (var i in clientMap) {
+						userList.push(clientMap[i]);
+					}
+					return userList;
+				})(),
+				msgList : msgQueue.getAll(),
+				figureList : figureQueue.getAll()
+			});
+
+			socket.broadcast.emit('user add', {'users' : userData, reconnect: data.reconnect});
+
+			socket.on('message send', function(data) {
+				var  msgTarget = data.msgTarget;
+				var isReply = data.isReply;
+				var msg = data.msg;
+				if (msg.length > 2048) {
+					msg = msg.substring(0, 2048) + '...';
 				}
-			} else {
-				socket.broadcast.emit('message push', sendMsg);
-				msgQueue.add(sendMsg);
-			}
-		});
 
-		socket.on('figure send', function(data) {
-			socket.broadcast.emit('figure push', data);
-			figureQueue.add(data);
-		});
+				var client = clientMap[socket.id];
 
-		socket.on('message delete', function(data) {
-			socket.emit('message delete', data);
-			socket.broadcast.emit('message delete', data);
-			msgQueue.delete(data);
+				var sendMsg = {
+					'isPrivate' : (msgTarget != null && '' != msgTarget),
+					'msgTarget' : msgTarget,
+					'isReply' : isReply,
+					'time' : new Date().getTime(),
+					'id' : client.id,
+					'name' : client.name,
+					'host' : client.host,
+					'addr' : client.addr,
+					'msg' : '' + msg
+				};
+
+				socket.emit('message push', sendMsg);
+				if (sendMsg.isPrivate) {
+					var targetSocket = io.sockets.socket(msgTarget);
+					if (clientMap[msgTarget] != null && targetSocket != null) {
+						var callbackCatched = false;
+						targetSocket.emit('message push', sendMsg, function(callbackData) {
+							callbackCatched = true;
+						});
+						setTimeout(function() {
+							//console.log('callbackCatched:'+callbackCatched);
+							if (!callbackCatched) {
+								socket.emit('error push', {
+									errorID : 'PRIVATEMSG_CALLBACK_UNCATCHED'
+								});
+							}
+						}, 10*1000);
+					} else {
+						socket.emit('error push', {
+							errorID : 'PRIVATEMSG_TARGET_NOT_EXIST'
+						});
+					}
+				} else {
+					socket.broadcast.emit('message push', sendMsg);
+					msgQueue.add(sendMsg);
+				}
+			});
+
+			socket.on('figure send', function(data) {
+				socket.broadcast.emit('figure push', data);
+				figureQueue.add(data);
+			});
+
+			socket.on('message delete', function(data) {
+				socket.emit('message delete', data);
+				socket.broadcast.emit('message delete', data);
+				msgQueue.delete(data);
+			});
 		});
 	});
 
